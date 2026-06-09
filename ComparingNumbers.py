143d45e7d69743cab6065c7a1afa96c2dabfcc62
KSCoreLabs <kscorelabs@gmail.com>
2026-06-10 01:18:00 +0530

    Added New Functions New Comparing Three Numbers

---
+++ ComparingNumbers.py
@@ -0,0 +1,14 @@
+a=int(input("Enter 1st number"))
+b=int(input("enter 2nd number"))
+c=int(input("Enter 3rd Number "))
+print("A=",a,"B=",b,"C=",c)
+if a>b:
+    if a>c:
+        print("A is greater")
+    else:
+        print("C is Gratest")
+elif b>a:
+    if b>c:
+        print("B is Gratest ")
+    else:
+        print("C is Gratest")
